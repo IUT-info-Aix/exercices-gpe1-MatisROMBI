c53360958770abe0b0ff2ed82fa6d78ca745c319
rombiMatis <matis.rombi@etu.univ-amu.fr>
2025-04-29 10:06:42 +0200

    Création de la fenêtre et des boutons

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,11 +1,15 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Palette extends Application {
@@ -25,6 +29,35 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Définition des paramètres de la fenêtre
+        Parameters params = getParameters();
+        int width = 600;
+        int height = 400;
+
+        // création d'une scène
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, width, height);
+
+        //Défintion de la scène principale
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Palette");
+
+        //Défintion des buttons
+        vert = new Button("Vert");
+        rouge = new Button("Rouge");
+        bleu = new Button("Bleu");
+
+        //Groupage des boutons dans une HBox "RGB"
+        HBox RGB = new HBox(vert, rouge, bleu);
+        RGB.setSpacing(10);
+        BorderPane.setMargin(RGB, new Insets(0, 0, 10, 0));
+
+        //Ajout de la VBox "RGB" dans le bas de la page
+        root.setBottom(RGB);
+        RGB.setAlignment(Pos.CENTER);
+
+
+        // Affichage de la fenêtre
         primaryStage.show();
     }
 }
