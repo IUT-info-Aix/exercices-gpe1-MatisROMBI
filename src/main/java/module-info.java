233a3ba862557846e8a7945f91e9fd988840e483
rombiMatis <matis.rombi@etu.univ-amu.fr>
2025-05-05 10:08:29 +0200

    Exercice introduction à FXML terminé

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-open module tp.intro.javafx {
+ module tp.intro.javafx {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
@@ -6,6 +6,9 @@ open module tp.intro.javafx {
     requires java.desktop;
     exports com.example.partie1;
     exports com.example.partie2;
+    opens com.example.partie3 to javafx.fxml;
+    exports com.example.partie3;
+
 }
 
 
