9ed52ea93b2ba2de760864a4e36f8e3483cf2c89
rombiMatis <matis.rombi@etu.univ-amu.fr>
2025-05-05 11:53:14 +0200

    Exercice 7

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,8 @@
     exports com.example.partie2;
     opens com.example.partie3 to javafx.fxml;
     exports com.example.partie3;
+    opens fr.amu.iut.exercice7 to javafx.fxml;
+    exports fr.amu.iut.exercice7;
 
 }
 
