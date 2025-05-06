ce08d8747c826842860b2bd59321b4ba46832d06
rombiMatis <matis.rombi@etu.univ-amu.fr>
2025-05-06 16:09:28 +0200

    Modification de okClicked et définition de cancelClicked

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -7,8 +7,8 @@ import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
 
-    private int nbEtoiles;
-    private String mdp = "";
+    private int nbEtoiles = 0;
+    private String mdp = " ";
 
     @FXML
     private TextField userName;
@@ -18,15 +18,18 @@ public class LoginControl extends GridPane {
 
     @FXML
     private void okClicked() {
-        System.out.println(userName.getText());
+        System.out.println("Nom d'utilisateur : " + userName.getText());
         nbEtoiles = pwd.getText().length();
         for (int i = 0; i < nbEtoiles; i++) {
             mdp = mdp + ("*");
         }
-        System.out.println(mdp);
+        System.out.println("Mot de passe : " + mdp);
+        mdp = "";
     }
 
     @FXML
     private void cancelClicked() {
+        userName.clear();
+        pwd.clear();
     }
 }
